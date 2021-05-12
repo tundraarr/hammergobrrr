186f6651c0019afad67fe88964085a4649cc1ffe
grammar15016 <81835682+grammar15016@users.noreply.github.com>
2021-05-13 00:26:59 +1200

    Added game over screen
    Also moved my files into my folder

--- Assets/Justin - Menu/PlayerCollision.cs
+++ Assets/Justin - Menu/PlayerCollision.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public GameObject gameOverUI;
+    public GameObject score;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
             Debug.Log("Player collided, dies");
+            gameOverUI.SetActive(true);
             gameObject.SetActive(false);
-            SceneManager.LoadScene("LiamScene 1");
         }        
     }
 }
